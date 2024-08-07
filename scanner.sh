b28b51db6db498012fb1cfb7d5158ebfb679de57
“zakarialag” <zakarialaganimes1@gmail.com>
2024-08-07 09:10:06 -0400

    scanner

--- scanner.sh
+++ scanner.sh
@@ -1,8 +1,7 @@
 #!/bin/bash
 
-# Run SonarQube Scanner
 mvn clean verify sonar:sonar \
   -Dsonar.projectKey=test \
   -Dsonar.projectName='test' \
   -Dsonar.host.url=https://sonarkube.ntdc.fr \
-  -Dsonar.token=test
+  -Dsonar.token=sqp_43710482cf101e103e997f8fe896dc1333789a69
